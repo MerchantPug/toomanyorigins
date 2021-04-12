93dbe665af13819c6205555e4e94034776dd9796
MerchantPug <batterypug@gmail.com>
2021-04-12 13:44:56 +1000

    Fixed server crash

--- src/main/java/io/github/merchantpug/toomanyorigins/networking/packet/RocketJumpPacket.java
+++ src/main/java/io/github/merchantpug/toomanyorigins/networking/packet/RocketJumpPacket.java
@@ -1,16 +1,14 @@
 package io.github.merchantpug.toomanyorigins.networking.packet;
 
-import io.github.apace100.origins.component.OriginComponent;
 import io.github.apace100.origins.mixin.DamageSourceAccessor;
 import io.github.merchantpug.toomanyorigins.TooManyOrigins;
-import io.github.merchantpug.toomanyorigins.power.RocketJumpPower;
-import io.github.merchantpug.toomanyorigins.registry.TMOComponents;
 import io.github.merchantpug.toomanyorigins.registry.TMODamageSources;
 import io.github.merchantpug.toomanyorigins.registry.TMOEffects;
 import io.netty.buffer.Unpooled;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
-import net.minecraft.entity.damage.DamageRecord;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.MinecraftServer;
@@ -18,16 +16,14 @@ import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.registry.Registry;
 import net.minecraft.world.explosion.Explosion;
-
 
 public class RocketJumpPacket {
     public static final Identifier ID = new Identifier(TooManyOrigins.MODID, "rocket_jump");
 
     public static void send(DamageSource damageSource, float damageAmount, double speed) {
         PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer());
-        buf.writeString(damageSource.getName());
+        buf.writeString(damageSource.getName(), 325);
         buf.writeBoolean(damageSource.bypassesArmor());
         buf.writeBoolean(damageSource.isFire());
         buf.writeBoolean(damageSource.isUnblockable());
@@ -39,7 +35,7 @@ public class RocketJumpPacket {
     }
 
     public static void handle(MinecraftServer server, ServerPlayerEntity player, ServerPlayNetworkHandler network, PacketByteBuf buf, PacketSender sender) {
-        DamageSource damageSource = DamageSourceAccessor.createDamageSource(buf.readString());
+        DamageSource damageSource = DamageSourceAccessor.createDamageSource(buf.readString(32767));
         DamageSourceAccessor accessor = (DamageSourceAccessor)damageSource;
         if (buf.readBoolean()) {
             accessor.callSetBypassesArmor();
@@ -64,8 +60,8 @@ public class RocketJumpPacket {
             float f = MathHelper.sin(player.yaw * 0.017453292F) * MathHelper.cos(player.pitch * 0.017453292F);
             float g = MathHelper.sin(player.pitch * 0.017453292F);
             float h = -MathHelper.cos(player.yaw * 0.017453292F) * MathHelper.cos(player.pitch * 0.017453292F);
-            player.damage((DamageSource)accessor, damageAmount);
-            player.world.createExplosion(player, TMODamageSources.jumpExplosion(player), null, player.getX(), player.getY(), player.getZ(), f, false, Explosion.DestructionType.NONE);
+            player.damage(damageSource, damageAmount);
+            player.world.createExplosion(player, TMODamageSources.jumpExplosion(player), null, player.getX(), player.getY(), player.getZ(), e, false, Explosion.DestructionType.NONE);
             player.addVelocity(f * speed * d, g * speed * d * 0.8, h * speed * d);
             player.velocityModified = true;
             if (player.hasStatusEffect(TMOEffects.CHARGED)) {
