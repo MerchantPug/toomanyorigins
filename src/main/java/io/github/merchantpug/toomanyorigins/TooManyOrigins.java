05e014d1676632fc08c4612f054f19eab92c4e4d
MerchantPug <batterypug@gmail.com>
2021-04-13 22:54:03 +1000

    Added initalization message

--- src/main/java/io/github/merchantpug/toomanyorigins/TooManyOrigins.java
+++ src/main/java/io/github/merchantpug/toomanyorigins/TooManyOrigins.java
@@ -1,11 +1,11 @@
 package io.github.merchantpug.toomanyorigins;
 
-import io.github.merchantpug.toomanyorigins.networking.packet.LightUpBlockEntityPacket;
+import io.github.merchantpug.toomanyorigins.networking.packet.LightUpBlockPacket;
 import io.github.merchantpug.toomanyorigins.networking.packet.RocketJumpPacket;
 import io.github.merchantpug.toomanyorigins.registry.*;
-import io.github.merchantpug.toomanyorigins.registry.conditions.TMOEntityConditions;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.fabricmc.loader.api.FabricLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -15,13 +15,15 @@ public class TooManyOrigins implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		LOGGER.info("TooManyOrigins is initializing. Enjoy!");
+
 		TMOBlocks.register();
 		TMOEntityConditions.register();
 		TMOEffects.register();
 		TMOEntities.register();
 		TMOItems.register();
 		TMOPowers.init();
-		ServerPlayNetworking.registerGlobalReceiver(LightUpBlockEntityPacket.ID, LightUpBlockEntityPacket::handle);
+		ServerPlayNetworking.registerGlobalReceiver(LightUpBlockPacket.ID, LightUpBlockPacket::handle);
 		ServerPlayNetworking.registerGlobalReceiver(RocketJumpPacket.ID, RocketJumpPacket::handle);
 	}
 }
