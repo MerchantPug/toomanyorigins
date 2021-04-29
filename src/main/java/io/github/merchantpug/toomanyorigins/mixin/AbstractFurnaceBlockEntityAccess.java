bce53f7031cb5d126b32ade61ec349624a1fd598
MerchantPug <batterypug@gmail.com>
2021-04-30 05:27:47 +1000

    Renamed files

--- src/main/java/io/github/merchantpug/toomanyorigins/mixin/AbstractFurnaceBlockEntityAccess.java
+++ src/main/java/io/github/merchantpug/toomanyorigins/mixin/AbstractFurnaceBlockEntityAccess.java
@@ -6,7 +6,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(AbstractFurnaceBlockEntity.class)
-public interface AbstractFurnaceBlockEntityAccessor {
+public interface AbstractFurnaceBlockEntityAccess {
     @Accessor
     int getBurnTime();
 
--- src/main/java/io/github/merchantpug/toomanyorigins/mixin/LivingEntityAccess.java
+++ src/main/java/io/github/merchantpug/toomanyorigins/mixin/LivingEntityAccess.java
@@ -2,10 +2,14 @@ package io.github.merchantpug.toomanyorigins.mixin;
 
 import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(LivingEntity.class)
-public interface LivingEntityInvoker {
+public interface LivingEntityAccess {
+    @Accessor("dead")
+    boolean getDead();
+
     @Invoker("isOnSoulSpeedBlock")
     boolean invokeIsOnSoulSpeedBlock();
 }
