6ea59504ce13136d936cbd6abf6acf3fac85cb14
MerchantPug <batterypug@gmail.com>
2021-04-30 05:30:17 +1000

    Removed unnecessary imports

--- src/main/java/io/github/merchantpug/toomanyorigins/registry/TMOEntityConditions.java
+++ src/main/java/io/github/merchantpug/toomanyorigins/registry/TMOEntityConditions.java
@@ -1,34 +1,19 @@
 package io.github.merchantpug.toomanyorigins.registry;
 
-import com.jamieswhiteshirt.reachentityattributes.ReachEntityAttributes;
 import io.github.apace100.origins.power.factory.condition.ConditionFactory;
 import io.github.apace100.origins.registry.ModRegistries;
 import io.github.apace100.origins.util.Comparison;
 import io.github.apace100.origins.util.SerializableData;
 import io.github.apace100.origins.util.SerializableDataType;
-import io.github.merchantpug.toomanyorigins.networking.packet.LightUpBlockPacket;
-import net.minecraft.block.AbstractFurnaceBlock;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.CampfireBlock;
-import net.minecraft.block.pattern.CachedBlockPosition;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayerEntity;
+import io.github.merchantpug.toomanyorigins.util.TMOSerializableDataType;
+import net.minecraft.entity.EntityGroup;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import io.github.merchantpug.toomanyorigins.TooManyOrigins;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.network.ServerPlayerInteractionManager;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.hit.HitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.world.RaycastContext;
 
 public class TMOEntityConditions {
-    @SuppressWarnings("unchecked")
     public static void register() {
         register(new ConditionFactory<>(new Identifier(TooManyOrigins.MODID, "nearby_entities"), new SerializableData()
                 .add("entity_type", SerializableDataType.ENTITY_TYPE)
@@ -38,14 +23,16 @@ public class TMOEntityConditions {
                 (data, player) -> {
                     EntityType<?> entityType = (EntityType<?>) data.get("entity_type");
                     Float playerBoxMultiplier = (Float) data.get("player_box_multiplier");
-                    int amount = player.world.getOtherEntities(player, player.getBoundingBox().expand(playerBoxMultiplier), entity -> {
-                        return entity.getType() == entityType;
-                    }).size();
+                    int amount = player.world.getOtherEntities(player, player.getBoundingBox().expand(playerBoxMultiplier), entity -> entity.getType() == entityType).size();
                     Comparison comparison = ((Comparison) data.get("comparison"));
                     int compareTo = data.getInt("compare_to");
-
                     return comparison.compare(amount, compareTo);
                 }));
+
+        register(new ConditionFactory<>(new Identifier(TooManyOrigins.MODID, "entity_group"), new SerializableData()
+                .add("group", TMOSerializableDataType.TMO_ENTITY_GROUP),
+                (data, entity) ->
+                        entity.getGroup() == (EntityGroup)data.get("group")));
     }
 
     private static void register(ConditionFactory<LivingEntity> conditionFactory) {
