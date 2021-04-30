5412e064f6867ebb24ff79fcd2b67889df211059
MerchantPug <batterypug@gmail.com>
2021-05-01 01:33:33 +1000

    Added more to Undead

---
+++ src/main/java/io/github/merchantpug/toomanyorigins/power/VisualTimerPower.java
@@ -0,0 +1,34 @@
+package io.github.merchantpug.toomanyorigins.power;
+
+import io.github.apace100.origins.power.CooldownPower;
+import io.github.apace100.origins.power.PowerType;
+import io.github.apace100.origins.util.HudRender;
+import io.github.merchantpug.toomanyorigins.registry.TMOComponents;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
+
+public class VisualTimerPower extends CooldownPower {
+    private final boolean resetOnRespawn;
+
+    public VisualTimerPower(PowerType<?> type, PlayerEntity player, int cooldownDuration, HudRender hudRender, boolean resetOnRespawn) {
+        super(type, player, cooldownDuration, hudRender);
+        this.resetOnRespawn = resetOnRespawn;
+    }
+
+    @Override
+    public void onChosen(boolean isOrbOfOrigin) {
+        super.onChosen(isOrbOfOrigin);
+        if (isOrbOfOrigin) {
+            ServerPlayerEntity serverPlayer = (ServerPlayerEntity)player;
+            this.use();
+            TMOComponents.setTimeAlive(serverPlayer, 0);
+        }
+    }
+
+    @Override
+    public void onRespawn() {
+        if (canUse() && resetOnRespawn) {
+            this.use();
+        }
+    }
+}
