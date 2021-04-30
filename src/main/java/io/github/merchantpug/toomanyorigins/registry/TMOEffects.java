d74f90315e8b045a7e327c6a29b0ba7b5066fd28
MerchantPug <batterypug@gmail.com>
2021-05-01 04:44:44 +1000

    Buffed Charged speed increase

--- src/main/java/io/github/merchantpug/toomanyorigins/registry/TMOEffects.java
+++ src/main/java/io/github/merchantpug/toomanyorigins/registry/TMOEffects.java
@@ -11,7 +11,7 @@ import net.minecraft.util.registry.Registry;
 import io.github.merchantpug.toomanyorigins.TooManyOrigins;
 
 public class TMOEffects {
-    public static final StatusEffect CHARGED = new ChargedStatusEffect().addAttributeModifier(EntityAttributes.GENERIC_MOVEMENT_SPEED, "c12451f1-b2a4-47aa-88ef-3f11b1b21e5e", 0.20000000298023224D, EntityAttributeModifier.Operation.MULTIPLY_TOTAL);;
+    public static final StatusEffect CHARGED = new ChargedStatusEffect().addAttributeModifier(EntityAttributes.GENERIC_MOVEMENT_SPEED, "c12451f1-b2a4-47aa-88ef-3f11b1b21e5e", 0.25D, EntityAttributeModifier.Operation.MULTIPLY_TOTAL);;
     public static final StatusEffect WITHER_RESISTANCE = new WitherResistanceStatusEffect();
     public static final StatusEffect ZOMBIFYING = new ZombifyingStatusEffect();
 
