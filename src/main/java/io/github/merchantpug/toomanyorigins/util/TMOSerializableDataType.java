cff582a3be1f065fa35773f7642ed83a27606b28
MerchantPug <batterypug@gmail.com>
2021-05-01 01:38:17 +1000

    Added player_undead entity group

--- src/main/java/io/github/merchantpug/toomanyorigins/util/TMOSerializableDataType.java
+++ src/main/java/io/github/merchantpug/toomanyorigins/util/TMOSerializableDataType.java
@@ -9,6 +9,7 @@ import net.minecraft.entity.EntityGroup;
 public class TMOSerializableDataType {
     public static final SerializableDataType<EntityGroup> TMO_ENTITY_GROUP =
             SerializableDataType.mapped(EntityGroup.class, HashBiMap.create(ImmutableMap.of(
-                    "smiteable", TMOEntityGroups.SMITEABLE
+                    "smiteable", TMOEntityGroups.SMITEABLE,
+                    "player_undead", TMOEntityGroups.PLAYER_UNDEAD
             )));
 }
