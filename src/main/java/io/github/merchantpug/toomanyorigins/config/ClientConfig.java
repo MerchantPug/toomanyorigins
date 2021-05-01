5c17b4f796f501fa1749c68f8402913655e9ec9d
MerchantPug <batterypug@gmail.com>
2021-05-01 16:24:30 +1000

    Added Overlay when player has Wither Resistance

--- src/main/java/io/github/merchantpug/toomanyorigins/config/ClientConfig.java
+++ src/main/java/io/github/merchantpug/toomanyorigins/config/ClientConfig.java
@@ -3,8 +3,22 @@ package io.github.merchantpug.toomanyorigins.config;
 import io.github.merchantpug.toomanyorigins.TooManyOrigins;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
 @Config(name = TooManyOrigins.MODID)
 public class ClientConfig implements ConfigData {
     public boolean showPlayerOverlays = true;
+    public boolean showWitherShieldOverlay = true;
+
+    @ConfigEntry.BoundedDiscrete(max = 1)
+    public float witherShieldOverlayStrength = 0.15F;
+
+    @Override
+    public void validatePostLoad() {
+        if (witherShieldOverlayStrength < 0F) {
+            witherShieldOverlayStrength = 0F;
+        } else if (witherShieldOverlayStrength > 1F) {
+            witherShieldOverlayStrength = 1F;
+        }
+    }
 }
---
+++ src/main/java/io/github/merchantpug/toomanyorigins/mixin/GameRendererMixin.java
@@ -0,0 +1,45 @@
+package io.github.merchantpug.toomanyorigins.mixin;
+
+import io.github.apace100.origins.OriginsClient;
+import io.github.apace100.origins.component.OriginComponent;
+import io.github.merchantpug.toomanyorigins.config.ClientConfig;
+import io.github.merchantpug.toomanyorigins.registry.TMOEffects;
+import io.github.merchantpug.toomanyorigins.registry.TMOPowers;
+import me.shedaniel.autoconfig.AutoConfig;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.entity.effect.StatusEffects;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+@Mixin(GameRenderer.class)
+public abstract class GameRendererMixin {
+    @Shadow @Final private MinecraftClient client;
+
+
+    @Shadow protected abstract void method_31136(float f);
+
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;lerp(FFF)F"))
+    private void drawWitherShieldOverlay(float tickDelta, long startTime, boolean tick, CallbackInfo ci) {
+        ClientConfig config = AutoConfig.getConfigHolder(ClientConfig.class).getConfig();
+        if(this.client.player.hasStatusEffect(TMOEffects.WITHER_RESISTANCE) && !this.client.player.hasStatusEffect(StatusEffects.NAUSEA) && config.showWitherShieldOverlay) {
+            this.method_31136(config.witherShieldOverlayStrength);
+        }
+    }
+
+    @ModifyArgs(method = "method_31136", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;color4f(FFFF)V", ordinal = 0))
+    private void modifyColor(Args args) {
+        if(TMOPowers.UNDEAD_RESISTANCE.isActive(this.client.player) && !this.client.player.hasStatusEffect(StatusEffects.NAUSEA)) {
+            args.set(0, (float)0.69411764705D);
+            args.set(1, (float)0.76078431372D);
+            args.set(2, (float)0.47058823529D);
+        }
+    }
+}
