540465e37f40b55ba81b1e63e3ad4393cf0fcdc8
MerchantPug <batterypug@gmail.com>
2021-05-19 17:55:37 +1000

    Removed unnecessary imports

--- src/main/java/io/github/merchantpug/toomanyorigins/mixin/EntityMixin.java
+++ src/main/java/io/github/merchantpug/toomanyorigins/mixin/EntityMixin.java
@@ -11,7 +11,6 @@ import net.minecraft.server.command.CommandOutput;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Nameable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
