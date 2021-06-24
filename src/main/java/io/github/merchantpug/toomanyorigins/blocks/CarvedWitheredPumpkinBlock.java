aeeeb8cf0ada6f8e7686cbda1ca9791863647189
Pug <81167232+MerchantPug@users.noreply.github.com>
2021-06-24 15:39:58 +1000

    Removed casts

--- src/main/java/io/github/merchantpug/toomanyorigins/blocks/CarvedWitheredPumpkinBlock.java
+++ src/main/java/io/github/merchantpug/toomanyorigins/blocks/CarvedWitheredPumpkinBlock.java
@@ -1,7 +1,6 @@
 package io.github.merchantpug.toomanyorigins.blocks;
 
 import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.BlockState;
 import net.minecraft.block.CarvedPumpkinBlock;
 import net.minecraft.util.math.Direction;
 
@@ -9,6 +8,6 @@ public class CarvedWitheredPumpkinBlock extends CarvedPumpkinBlock {
 
     public CarvedWitheredPumpkinBlock(AbstractBlock.Settings settings) {
         super(settings);
-        this.setDefaultState((BlockState)((BlockState)this.stateManager.getDefaultState()).with(FACING, Direction.NORTH));
+        this.setDefaultState(this.stateManager.getDefaultState().with(FACING, Direction.NORTH));
     }
 }
