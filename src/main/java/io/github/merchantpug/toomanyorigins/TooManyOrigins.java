8a4bcda95ca50f455a40c978f2397dbb969736f1
Pug <81167232+MerchantPug@users.noreply.github.com>
2022-01-06 08:16:14 +1100

    Optimised imports

--- src/main/java/io/github/merchantpug/toomanyorigins/TooManyOrigins.java
+++ src/main/java/io/github/merchantpug/toomanyorigins/TooManyOrigins.java
@@ -1,17 +1,11 @@
 package io.github.merchantpug.toomanyorigins;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 import io.github.apace100.apoli.util.NamespaceAlias;
 import io.github.merchantpug.toomanyorigins.registry.*;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.ArrayList;
 
 public class TooManyOrigins implements ModInitializer {
 	public static final String MODID = "toomanyorigins";
