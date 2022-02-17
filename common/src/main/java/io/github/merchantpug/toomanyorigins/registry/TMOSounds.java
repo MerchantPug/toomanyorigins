1e4e11760e7c7bc941a21e06ac2d7942e6b3a113
Pug <81167232+MerchantPug@users.noreply.github.com>
2022-02-18 00:17:51 +1100

    Changed identifier to use the sound event's ID

--- common/src/main/java/io/github/merchantpug/toomanyorigins/registry/TMOSounds.java
+++ common/src/main/java/io/github/merchantpug/toomanyorigins/registry/TMOSounds.java
@@ -2,13 +2,12 @@ package io.github.merchantpug.toomanyorigins.registry;
 
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 public class TMOSounds {
 
     public static final SoundEvent ORIGIN_HARE_DASH = new SoundEvent(new Identifier("toomanyorigins", "origin.hare.dash"));
 
     public static void register() {
-        TMORegistriesArchitectury.SOUNDS.register(new Identifier("toomanyorigins", "origin.hare.dash"), () -> ORIGIN_HARE_DASH);
+        TMORegistriesArchitectury.SOUNDS.register(ORIGIN_HARE_DASH.getId(), () -> ORIGIN_HARE_DASH);
     }
 }
