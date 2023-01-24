28c3356ea59273d5a5c7d27d4edd3f2339941ca9
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2023-01-25 09:30:16 +1100

    Remove unused imports

--- src/main/java/com/github/merchantpug/toomanyorigins/integration/TMOModMenuIntegration.java
+++ src/main/java/com/github/merchantpug/toomanyorigins/integration/TMOModMenuIntegration.java
@@ -3,9 +3,7 @@ package com.github.merchantpug.toomanyorigins.integration;
 import com.github.merchantpug.toomanyorigins.TooManyOrigins;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import com.github.merchantpug.toomanyorigins.util.TooManyOriginsConfig;
 import eu.midnightdust.lib.config.MidnightConfig;
-import me.shedaniel.autoconfig.AutoConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
