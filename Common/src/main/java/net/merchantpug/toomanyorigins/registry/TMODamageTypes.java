4f6e68638d83136e9e396a39077c64f863d04aac
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2023-06-14 04:30:26 +1000

    More 1.19.4 specifics.

--- Common/src/main/java/net/merchantpug/toomanyorigins/registry/TMODamageTypes.java
+++ Common/src/main/java/net/merchantpug/toomanyorigins/registry/TMODamageTypes.java
@@ -8,9 +8,9 @@ import net.minecraft.world.damagesource.DamageType;
 import net.minecraft.world.entity.Entity;
 
 public class TMODamageTypes {
+    public static final ResourceKey<DamageType> BUILDUP = ResourceKey.create(Registries.DAMAGE_TYPE, TooManyOrigins.asResource("buildup"));
     public static final ResourceKey<DamageType> DRAGON_MAGIC = ResourceKey.create(Registries.DAMAGE_TYPE, TooManyOrigins.asResource("dragon_magic"));
     public static final ResourceKey<DamageType> INDIRECT_DRAGON_MAGIC = ResourceKey.create(Registries.DAMAGE_TYPE, TooManyOrigins.asResource("indirect_dragon_magic"));
-    public static final ResourceKey<DamageType> OVERHEAT = ResourceKey.create(Registries.DAMAGE_TYPE, TooManyOrigins.asResource("overheat"));
     public static final ResourceKey<DamageType> ZOMBIFICATION = ResourceKey.create(Registries.DAMAGE_TYPE, TooManyOrigins.asResource("zombification"));
 
     public static DamageSource indirectDragonMagic(Entity source, Entity attacker) {
