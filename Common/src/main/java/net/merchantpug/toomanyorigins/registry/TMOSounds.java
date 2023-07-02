b94c13c188eeeb563a7b73e025ddbdaf12a51e32
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2023-07-02 14:49:23 +1000

    Hare rework

--- Common/src/main/java/net/merchantpug/toomanyorigins/registry/TMOSounds.java
+++ Common/src/main/java/net/merchantpug/toomanyorigins/registry/TMOSounds.java
@@ -4,15 +4,15 @@ import net.merchantpug.toomanyorigins.TooManyOrigins;
 import net.merchantpug.toomanyorigins.registry.services.RegistrationProvider;
 import net.merchantpug.toomanyorigins.registry.services.RegistryObject;
 import net.minecraft.core.Registry;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.sounds.SoundEvent;
 
 import java.util.function.Supplier;
 
 public class TMOSounds {
-    private static final RegistrationProvider<SoundEvent> SOUND_EVENT = RegistrationProvider.get(Registries.SOUND_EVENT, TooManyOrigins.MOD_ID);
+    private static final RegistrationProvider<SoundEvent> SOUND_EVENT = RegistrationProvider.get(Registry.SOUND_EVENT, TooManyOrigins.MOD_ID);
 
-    public static final RegistryObject<SoundEvent> ORIGIN_HARE_DASH = register("origin.hare.dash", () -> SoundEvent.createVariableRangeEvent(TooManyOrigins.asResource("origin.hare.dash")));
+    public static final RegistryObject<SoundEvent> ORIGIN_HARE_MAX_SPEED = register("origin.hare.max_speed", () -> new SoundEvent(TooManyOrigins.asResource("origin.hare.max_speed")));
+    public static final RegistryObject<SoundEvent> ORIGIN_HARE_DASH = register("origin.hare.dash", () -> new SoundEvent(TooManyOrigins.asResource("origin.hare.dash")));
 
     public static void register() {
     }
