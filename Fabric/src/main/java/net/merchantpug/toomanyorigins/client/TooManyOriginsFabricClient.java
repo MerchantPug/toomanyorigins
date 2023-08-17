8dfaffc156e9ee0aceadc2aa3b3dc91f26905e56
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2023-08-17 11:40:31 +1000

    Remove MidnightLib inclusion, remove unused field.

--- Fabric/src/main/java/net/merchantpug/toomanyorigins/client/TooManyOriginsFabricClient.java
+++ Fabric/src/main/java/net/merchantpug/toomanyorigins/client/TooManyOriginsFabricClient.java
@@ -1,13 +1,11 @@
 package net.merchantpug.toomanyorigins.client;
 
-import eu.midnightdust.core.MidnightLibClient;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
-import net.merchantpug.toomanyorigins.TooManyOrigins;
 import net.merchantpug.toomanyorigins.registry.TMOBlocks;
 import net.merchantpug.toomanyorigins.registry.TMOEntityTypes;
 import net.minecraft.client.renderer.RenderType;
@@ -15,7 +13,6 @@ import net.minecraft.client.renderer.entity.NoopRenderer;
 import net.minecraft.client.renderer.entity.ThrownItemRenderer;
 
 public class TooManyOriginsFabricClient implements ClientModInitializer {
-    public static boolean isServerRunningTMO = false;
 
     @Override
     @Environment(EnvType.CLIENT)
