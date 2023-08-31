d10437904f106516d4473fa5e5249a9fd951008a
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2023-08-31 10:36:53 +1000

    Update builder ids to include the toomanyorigins namespace.

--- Common/src/main/java/net/merchantpug/toomanyorigins/registry/TMOEntityTypes.java
+++ Common/src/main/java/net/merchantpug/toomanyorigins/registry/TMOEntityTypes.java
@@ -15,8 +15,8 @@ import java.util.function.Supplier;
 public class TMOEntityTypes {
     private static final RegistrationProvider<EntityType<?>> ENTITY_TYPES = RegistrationProvider.get(Registry.ENTITY_TYPE, TooManyOrigins.MOD_ID);
 
-    public static final RegistryObject<EntityType<SmallDragonFireballEntity>> SMALL_DRAGON_FIREBALL = register("small_dragon_fireball", () -> EntityType.Builder.<SmallDragonFireballEntity>of(SmallDragonFireballEntity::new, MobCategory.MISC).sized(0.25f, 0.25f).clientTrackingRange(64).updateInterval(10).build("small_dragon_fireball"));
-    public static final RegistryObject<EntityType<FireballAreaEffectCloudEntity>> FIREBALL_AREA_EFFECT_CLOUD = register("fireball_area_effect_cloud", () -> EntityType.Builder.<FireballAreaEffectCloudEntity>of(FireballAreaEffectCloudEntity::new, MobCategory.MISC).fireImmune().sized(6.0F, 0.5F).clientTrackingRange(10).updateInterval(2147483647).build("fireball_area_effect_cloud"));
+    public static final RegistryObject<EntityType<SmallDragonFireballEntity>> SMALL_DRAGON_FIREBALL = register("small_dragon_fireball", () -> EntityType.Builder.<SmallDragonFireballEntity>of(SmallDragonFireballEntity::new, MobCategory.MISC).sized(0.25f, 0.25f).clientTrackingRange(64).updateInterval(10).build("toomanyorigins:small_dragon_fireball"));
+    public static final RegistryObject<EntityType<FireballAreaEffectCloudEntity>> FIREBALL_AREA_EFFECT_CLOUD = register("fireball_area_effect_cloud", () -> EntityType.Builder.<FireballAreaEffectCloudEntity>of(FireballAreaEffectCloudEntity::new, MobCategory.MISC).fireImmune().sized(6.0F, 0.5F).clientTrackingRange(10).updateInterval(2147483647).build("toomanyorigins:fireball_area_effect_cloud"));
 
     public static void register() {
 
