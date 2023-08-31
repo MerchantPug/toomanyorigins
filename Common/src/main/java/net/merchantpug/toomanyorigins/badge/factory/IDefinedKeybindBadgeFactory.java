6efcd8080f8d790f7438fc08f895293ef69beda0
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2023-08-31 10:36:20 +1000

    Add accesswidener to Common module.

--- Common/src/main/java/net/merchantpug/toomanyorigins/badge/factory/IDefinedKeybindBadgeFactory.java
+++ Common/src/main/java/net/merchantpug/toomanyorigins/badge/factory/IDefinedKeybindBadgeFactory.java
@@ -1,13 +1,8 @@
 package net.merchantpug.toomanyorigins.badge.factory;
 
 import io.github.apace100.calio.data.SerializableData;
-import io.github.apace100.calio.data.SerializableDataType;
 import io.github.apace100.calio.data.SerializableDataTypes;
-import io.github.apace100.origins.badge.Badge;
 import net.merchantpug.apugli.platform.Services;
-import net.merchantpug.toomanyorigins.badge.IDefinedKeybindBadge;
-
-import java.util.function.Function;
 
 public interface IDefinedKeybindBadgeFactory extends IBadgeFactory {
 
