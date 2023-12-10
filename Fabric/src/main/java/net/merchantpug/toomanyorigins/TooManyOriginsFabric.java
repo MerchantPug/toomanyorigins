420d15beb33174151d6a0a31310b63aa22e56c94
Pug <81167232+MerchantPug@users.noreply.github.com>
2023-12-11 03:06:25 +1100

    Reimplement missing code on Fabric.

--- Fabric/src/main/java/net/merchantpug/toomanyorigins/TooManyOriginsFabric.java
+++ Fabric/src/main/java/net/merchantpug/toomanyorigins/TooManyOriginsFabric.java
@@ -1,9 +1,19 @@
 package net.merchantpug.toomanyorigins;
 
+import io.github.apace100.apoli.power.PowerTypes;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.merchantpug.toomanyorigins.data.LegacyContentManagerFabric;
+import net.merchantpug.toomanyorigins.data.LegacyContentRegistry;
+import net.merchantpug.toomanyorigins.network.s2c.SyncLegacyContentPacket;
 import net.merchantpug.toomanyorigins.registry.TMOItems;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.packs.PackType;
 import net.minecraft.world.item.CreativeModeTabs;
 
 public class TooManyOriginsFabric implements ModInitializer {
@@ -18,13 +28,30 @@ public class TooManyOriginsFabric implements ModInitializer {
 			if(VERSION.contains("-")) {
 				VERSION = VERSION.split("-")[0];
 			}
+			ResourceManagerHelper.registerBuiltinResourcePack(TooManyOrigins.asResource("legacytoomanyorigins"), modContainer, Component.translatable("dataPack.legacytoomanyorigins.name"), ResourcePackActivationType.NORMAL);
 		});
+
 		TooManyOrigins.LOG.info("TooManyOrigins " + VERSION + " is initializing. Enjoy!");
 		TooManyOrigins.init();
 
-		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.COMBAT).register(entries -> entries.accept(TMOItems.DRAGON_FIREBALL.get()));
-		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.NATURAL_BLOCKS).register(entries -> entries.accept(TMOItems.WITHERED_CROP_SEEDS.get()));
-		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.NATURAL_BLOCKS).register(entries -> entries.accept(TMOItems.WITHERED_STEM_SEEDS.get()));
+		ServerLifecycleEvents.SYNC_DATA_PACK_CONTENTS.register((player, joined) -> {
+			var packet = new SyncLegacyContentPacket(LegacyContentRegistry.isDragonFireballEnabled(), LegacyContentRegistry.areWitheredCropsEnabled(), LegacyContentRegistry.isZombifyingEffectEnabled());
+			ServerPlayNetworking.send(player, packet.getFabricId(), packet.toBuf());
+		});
+
+		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.COMBAT).register(entries -> {
+			if (!LegacyContentRegistry.isDragonFireballEnabled()) return;
+			entries.accept(TMOItems.DRAGON_FIREBALL.get());
+		});
+		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.NATURAL_BLOCKS).register(entries -> {
+			if (!LegacyContentRegistry.areWitheredCropsEnabled()) return;
+			entries.accept(TMOItems.WITHERED_CROP_SEEDS.get());
+			entries.accept(TMOItems.WITHERED_STEM_SEEDS.get());
+		});
+
+		ResourceManagerHelper.get(PackType.SERVER_DATA).registerReloadListener(new LegacyContentManagerFabric());
+
+		PowerTypes.DEPENDENCIES.add(TooManyOrigins.asResource("legacy_content"));
 	}
 
 }
--- Fabric/src/main/java/net/merchantpug/toomanyorigins/client/TooManyOriginsFabricClient.java
+++ Fabric/src/main/java/net/merchantpug/toomanyorigins/client/TooManyOriginsFabricClient.java
@@ -4,10 +4,13 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
+import net.merchantpug.toomanyorigins.client.particle.CustomDragonBreathParticle;
 import net.merchantpug.toomanyorigins.registry.TMOBlocks;
 import net.merchantpug.toomanyorigins.registry.TMOEntityTypes;
+import net.merchantpug.toomanyorigins.registry.TMOParticleTypes;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.NoopRenderer;
 import net.minecraft.client.renderer.entity.ThrownItemRenderer;
@@ -22,6 +25,8 @@ public class TooManyOriginsFabricClient implements ClientModInitializer {
 
         EntityRendererRegistry.register(TMOEntityTypes.SMALL_DRAGON_FIREBALL.get(), ThrownItemRenderer::new);
         EntityRendererRegistry.register(TMOEntityTypes.FIREBALL_AREA_EFFECT_CLOUD.get(), NoopRenderer::new);
+
+        ParticleFactoryRegistry.getInstance().register(TMOParticleTypes.CUSTOM_DRAGON_BREATH.get(), CustomDragonBreathParticle.Provider::new);
 
         ColorProviderRegistry.BLOCK.register((state, world, pos, tintIndex) -> {
             int k = 255;
